6f7e4dafdacea06e78c839490d0758ee9c7c9194
jchua21 <jchua@unsa.edu.pe>
2024-05-28 23:32:50 -0500

    Add java files

---
+++ QrCode.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package qrcode;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class QrCode {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        QR qr = new QR();
+        qr.setVisible(true);
+    }
+    
+}
